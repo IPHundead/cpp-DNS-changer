42233f281c7672af8b83f79126ff40499f464fec
Jibel <itsjibel@gmail.com>
2023-08-02 20:49:17 +0330

    Improve code smell

--- dns changer.cpp
+++ dns changer.cpp
@@ -1,72 +1,60 @@
-#include <windows.h>
 #include <iostream>
-using namespace std;
+#include <vector>
 
-const string mycmd="netsh interface ipv4 add dnsservers \"Wi-Fi\"";
+class dns_server
+{
+public:
+	std::string name;
+	std::string ip[2];
 
-struct dnsserver{
-	string name;
-	string ip[2];
+    void create(const std::string& name, const std::string& ip1, const std::string& ip2)
+    {
+        ip[0] = ip1;
+        ip[1] = ip2;
+        this->name = name;
+    }
 };
 
-void cleardns()
+void clear_dns()
 {
-	system("netsh interface ipv4 delete dnsservers \"Wi-Fi\" all");
+    system("netsh interface ipv4 delete dnsservers \"Wi-Fi\" all");
 }
 
-void changedns(string dns1, string dns2)
+void set_dns(dns_server server)
 {
-	string tempx;
-	
-	cleardns();
-	
-	tempx=mycmd+" "+dns1+" index=1";
-	system(tempx.c_str());
-	cout << "\n" << tempx;
-	
-	tempx=mycmd+" "+dns2+" index=2";
-	system(tempx.c_str());
-	cout << endl << tempx;
+    clear_dns();
+
+    const std::string mycmd="netsh interface ipv4 add dnsservers \"Wi-Fi\"";
+    for (std::string ip : server.ip)
+    {
+        std::string dnsSetterCommand (mycmd + " " + ip + " index=1");
+        system(dnsSetterCommand.c_str());
+    }
 }
 
 int main()
 {
-	dnsserver server[30];
+	std::vector<dns_server> servers(3);
 
-	string temp;
-	int n;	
-	
-	server[0].name="shecan";
-	server[0].ip[0]="178.22.122.100";
-	server[0].ip[1]="185.51.200.2";
-	
-	server[1].name="electro";
-	server[1].ip[0]="78.157.42.100";
-	server[1].ip[1]="78.157.42.101";
-	
-	server[2].name="radar game";
-	server[2].ip[0]="10.202.10.10";
-	server[2].ip[1]="10.202.10.11";
-	
-	
+	servers[0].create("shecan", "178.22.122.100", "185.51.200.2");
+	servers[1].create("electro", "78.157.42.100", "78.157.42.101");
+	servers[2].create("radar game", "10.202.10.10", "10.202.10.11");
 
-	while(true)
+	while (true)
 	{
-		system("CLS");
-		
-		cout << "0 to clear dns server, or choose your dns server:\n"; 
-		
-		cin >> n;
-		
-		if (n==0)
-		{
-			cleardns();	
-		}
-		else
-		{
-			n=n-1;
-			changedns(server[n].ip[0], server[n].ip[1]);
-		}
+		system("cls");
+		std::cout<<"0 to clear dns server, or choose your dns server:"<<std::endl;
+
+    	int n;
+		std::cin>>n;
+
+        if (n != 0)
+        {
+			n--;
+			set_dns(servers[n]);
+        } else
+			clear_dns();
 	}
 
+    return 0;
 }
