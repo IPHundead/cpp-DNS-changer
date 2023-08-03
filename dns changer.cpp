c33a722217e5a5d152252759afb5e9e1fd66dba3
Jibel <itsjibel@gmail.com>
2023-08-03 12:10:37 +0330

    Modify variable names

--- dns changer.cpp
+++ dns changer.cpp
@@ -24,35 +24,32 @@ void set_dns(dns_server server)
 {
     clear_dns();
 
-    const std::string mycmd="netsh interface ipv4 add dnsservers \"Wi-Fi\"";
+    const std::string add_dns_cmd ("netsh interface ipv4 add dnsservers \"Wi-Fi\"");
     for (std::string ip : server.ip)
     {
-        std::string dnsSetterCommand (mycmd + " " + ip + " index=1");
-        system(dnsSetterCommand.c_str());
+        std::string dns_setter_cmd (add_dns_cmd + " " + ip + " index=1");
+        system(dns_setter_cmd.c_str());
     }
 }
 
 int main()
 {
-	std::vector<dns_server> servers(3);
-
-	servers[0].create("shecan", "178.22.122.100", "185.51.200.2");
-	servers[1].create("electro", "78.157.42.100", "78.157.42.101");
-	servers[2].create("radar game", "10.202.10.10", "10.202.10.11");
+	std::vector<dns_server> dns_servers(3);
+	dns_servers[0].create("shecan", "178.22.122.100", "185.51.200.2");
+	dns_servers[1].create("electro", "78.157.42.100", "78.157.42.101");
+	dns_servers[2].create("radar game", "10.202.10.10", "10.202.10.11");
 
 	while (true)
 	{
 		system("cls");
+
 		std::cout<<"0 to clear dns server, or choose your dns server:"<<std::endl;
+    	int dns_server_number;
+		std::cin>>dns_server_number;
 
-    	int n;
-		std::cin>>n;
-
-        if (n != 0)
-        {
-			n--;
-			set_dns(servers[n]);
-        } else
+        if (dns_server_number != 0)
+			set_dns(dns_servers[dns_server_number-1]);
+        else
 			clear_dns();
 	}
 
