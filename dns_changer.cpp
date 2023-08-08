f1ea84424dbc2438119c346cc6cb3358cfb4afee
IPH_undead <110030986+IPHundead@users.noreply.github.com>
2023-08-08 21:11:36 +0330

    made changes to the UI.

---
+++ dns_changer.cpp
@@ -0,0 +1,16 @@
+#include "ui.hpp"
+
+int main()
+{
+    std::vector<DNSServer> DNSServers {
+    	{"Google", "8.8.8.8", "8.8.4.4"},
+    	{"CloudFlare", "1.1.1.1", "1.0.0.1"},
+        {"Shecan", "178.22.122.100", "185.51.200.2"},
+        {"Electro", "78.157.42.100", "78.157.42.101"},
+        {"RadarGame", "10.202.10.10", "10.202.10.11"},
+        {"OpenDNS", "208.67.222.222", "208.67.220.220"}
+    };
+
+    ui DNSChangerUI;
+    DNSChangerUI.run(DNSServers);
+}
